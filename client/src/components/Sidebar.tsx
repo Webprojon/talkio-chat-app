2457cc2c5f8017d9f82a9004796a68728c4ab1b0
Webprojon <tokhirjonkhasanov@gmail.com>
2025-07-13 10:18:04 +0200

    .

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,32 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 import useChatUser from "../store/chatUser";
+import { apiRequest } from "../lib/apiRequest";
 
 interface Chats {
 	id: number;
 	username: string;
-	lastMessage: string;
+	avatar: string;
 }
 
 export default function Sidebar() {
 	const [searchTerm, setSearchTerm] = useState<string>("");
 	const [chats, setChats] = useState<Chats[]>([]);
+	const [users, setUsers] = useState<Chats[]>([]);
 	const { setUser } = useChatUser();
 
-	const staticChats: Chats[] = [
-		{
-			id: 1,
-			username: "John Doe",
-			lastMessage: "Hi John",
-		},
-		{
-			id: 2,
-			username: "Alex",
-			lastMessage: "How are you doing?",
-		},
-	];
+	const fetchUsers = async () => {
+		try {
+			const res = await apiRequest("/users");
+			setUsers(res.data.data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-	const searchChats = searchTerm ? staticChats.filter((chat) => chat.username.toLowerCase().includes(searchTerm.toLowerCase())) : [];
+	const fetchChats = async () => {
+		try {
+			const res = await apiRequest("/chats", { withCredentials: true });
+			console.log(res.data.data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		fetchChats();
+		fetchUsers();
+	}, []);
+
+	const foundedUsers = searchTerm ? users.filter((chat) => chat.username.toLowerCase().includes(searchTerm.toLowerCase())) : [];
 
 	const handleSelectChat = (chat: Chats) => {
 		if (!chats.some((c) => c.id === chat.id)) {
@@ -36,9 +48,18 @@ export default function Sidebar() {
 		handleSendChatUser(chat);
 	};
 
-	const handleSendChatUser = (chat: Chats) => {
-		setUser(chat);
+	const handleSendChatUser = (user: Chats) => {
+		setUser(user);
 	};
+
+	//const handleOpenChat = async (id, receiver) => {
+	//	try {
+	//		const res = await apiRequest(`/chats/${id}`, { withCredentials: true });
+	//		setChat({ ...res.data, receiver });
+	//	} catch (err) {
+	//		console.log(err);
+	//	}
+	//};
 
 	return (
 		<div className="relative flex flex-col gap-4 sm:gap-2 flex-1 rounded-b-md border-r border-b p-3 bg-[#1C2029]">
@@ -58,39 +79,39 @@ export default function Sidebar() {
 
 			{searchTerm && (
 				<div className="absolute top-[9%] left-0 bg-[#1d222e] w-full h-[91%] sm:p-3">
-					{searchChats.map((chat) => (
+					{foundedUsers.map((user) => (
 						<div
-							key={chat.id}
-							onClick={() => handleSelectChat(chat)}
+							key={user.id}
+							onClick={() => handleSelectChat(user)}
 							className="flex items-center gap-3 rounded-sm px-2 py-3 sm:p-2 cursor-pointer transition-all bg-[#2e323b] hover:bg-[#2a2e38]"
 						>
-							<img src="./noavatar.png" alt="user img" className="w-9 h-9 rounded-full object-cover border" />
+							<img src={user.avatar || "./noavatar.png"} alt="user img" className="w-9 h-9 rounded-full object-cover border" />
 							<div className="flex flex-col gap-1">
-								<span className="sm:text-sm font-medium">{chat.username}</span>
-								<p className="text-sm sm:text-xs text-stone-400">{chat.lastMessage}</p>
+								<span className="sm:text-sm font-medium">{user.username}</span>
+								{/*<p className="text-sm sm:text-xs text-stone-400">{user.lastMessage}</p>*/}
 							</div>
 						</div>
 					))}
 
-					{searchChats.length === 0 && <div className="mt-4 text-center text-xs text-stone-300">No matching user found!</div>}
+					{foundedUsers.length === 0 && <div className="mt-4 text-center text-xs text-stone-300">No matching user found!</div>}
 				</div>
 			)}
 
 			{chats.map((chat) => (
 				<div
 					key={chat.id}
-					onClick={() => handleSendChatUser(chat)}
+					onClick={() => handleOpenChat}
 					className="flex items-center gap-3 rounded-sm px-2 py-3 sm:p-2 cursor-pointer transition-all bg-[#252932] hover:bg-[#2a2e38]"
 				>
-					<img src="./noavatar.png" alt="user img" className="w-9 h-9 rounded-full object-cover border" />
+					<img src={chat.avatar || "./noavatar.png"} alt="user img" className="w-9 h-9 rounded-full object-cover border" />
 					<div className="flex flex-col gap-1">
 						<span className="sm:text-sm font-medium">{chat.username}</span>
-						<p className="text-sm sm:text-xs text-stone-400">{chat.lastMessage}</p>
+						{/*<p className="text-sm sm:text-xs text-stone-400">{chat.lastMessage}</p>*/}
 					</div>
 				</div>
 			))}
 
-			{searchChats.length === 0 && chats.length === 0 && (
+			{foundedUsers.length === 0 && chats.length === 0 && (
 				<div className="mt-4 text-center text-xs text-stone-300">No chats yet, search for a friend to start a conversation!</div>
 			)}
 		</div>
