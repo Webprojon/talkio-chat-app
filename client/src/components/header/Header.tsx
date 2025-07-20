55726bf9c7263b52a97adc5d786492b9acc07adb
Webprojon <tokhirjonkhasanov@gmail.com>
2025-07-20 22:09:40 +0200

    fixed

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
 	const { activeChatUser } = useChatUserStore();
 
 	return (
-		<div className={`relative justify-between items-center border-b p-4 sm:p-2 rounded-t-md bg-[#1C2029] ${activeChatUser ? "hidden" : "flex"}`}>
+		<div className={`relative justify-between items-center border-b p-4 sm:p-2 rounded-t-md bg-[#1C2029] ${activeChatUser ? "hidden sm:flex" : "flex"}`}>
 			<div className="flex-center gap-3">
 				<img src={currentUser?.avatar || "./noavatar.png"} alt="user img" className="w-11 h-11 rounded-full object-cover border" />
 				<span className="text-lg font-medium">{currentUser?.username}</span>
